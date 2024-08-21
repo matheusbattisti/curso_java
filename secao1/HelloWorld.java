7dd39d54cfa45442994f953b6ae648a3407d6c86
Matheus Battisti <matheusbudkewicz@gmail.com>
2024-08-21 15:17:35 -0300

    first commit

---
+++ secao1/HelloWorld.java
@@ -0,0 +1,14 @@
+package secao1;
+
+/**
+ * HelloWorld
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Olá Mundo! 2");
+
+    }
+    
+}
